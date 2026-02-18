d196e2412d56323fc4d57a1fe5828f6f9c33ceb5
simpai <junghun.cha@gmail.com>
2026-02-19 01:59:51 +0900

    pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,33 +7,12 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: null,
       includeAssets: ['favicon.ico', 'pwa-192x192.png', 'pwa-512x512.png'],
-      manifest: {
-        name: 'Elevator Simulator',
-        short_name: 'Elevator',
-        description: 'Advanced Real-time Elevator Simulation',
-        theme_color: '#222222',
-        background_color: '#222222',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
+      manifest: false,
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,ttf,woff,woff2}'],
+      },
     })
   ],
 })
